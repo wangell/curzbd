44938c75c1e7e3b5b2aa255feedb21a6d7bc9328
William Angell <william.h.angell@gmail.com>
2014-04-21 18:09:46 -0400

    Added resizing for queue window, refactored fetching

--- curzbd/QueueWindow.py
+++ curzbd/QueueWindow.py
@@ -8,53 +8,45 @@ class QueueWindow(CurzbdWindow.CurzbdWindow):
     def __init__(self, stdscr, sab):
         self.sabnzbd = sab
         self.window = stdscr.derwin(2, 0)
-        self.selected_index = 0
-        self.headings = ["Status", "Filename", "Size", "Percent"]
-        self.sizes = [15, 30, 15, 10]
-
-        #Heading percentages
+        self.columns = [("Status",.05), ("Filename",.45), ("Size",.1), ("Progress",.1), ("Timeleft", .2)]
+        self.maxY,self.maxX = self.window.getmaxyx()
+        self.padding = 5
+        self.maxX -= (self.padding * len(self.columns))
 
     def update(self):
-        (maxY,maxX) = self.window.getmaxyx()
+        self.print_row(["Status", "Filename", "Size", "Progress", "Timeleft"], 0)
 
-        self.window.addstr(0, 0, "S")
-        self.window.addstr(0, 3, "Filename")
-        self.window.addstr(0, 48, "Size")
-        self.window.addstr(0, 57, "Progess")
-        self.window.addstr(0, 70, "Timeleft")
-
-        self.queued = []
-        
-        sabout = self.sabnzbd.queue_output([])
-        if "queue" in sabout:
-            queue = sabout["queue"]
-            self.queued = queue["slots"]
-
-        x = 1
-        index = 0
-        for q in self.queued:
-            if self.selected_index == index:
-                self.window.addstr(x, 0, q["status"][:1], curses.color_pair(3))
-                self.window.addstr(x, 3, q["filename"][:40], curses.color_pair(3))
-                self.window.addstr(x, 48, q["size"], curses.color_pair(3))
-                self.window.addstr(x, 57, q["percentage"] + "%", curses.color_pair(3))
-                self.window.addstr(x, 70, q["timeleft"], curses.color_pair(3))
-            else:
-                self.window.addstr(x, 0, q["status"][:1])
-                self.window.addstr(x, 3, q["filename"][:40])
-                self.window.addstr(x, 48, q["size"])
-                self.window.addstr(x, 57, q["percentage"] + "%")
-                self.window.addstr(x, 70, q["timeleft"])
-            x += 1
-            index += 1
+        q = self.fetch_queue()
+        for s in range(0, min(len(q), self.maxY -1)):
+            self.print_row(q[s],s+1)
 
     def fetch_queue(self):
-        pass
+        queue = self.sabnzbd.queue_output([])
+
+        queue_filtered = []
+        if "queue" in queue:
+            for q in queue["queue"]["slots"]:
+                temp_r = []
+                temp_r.append(q["status"])
+                temp_r.append(q["filename"])
+                temp_r.append(q["size"])
+                temp_r.append(q["percentage"] + "%")
+                temp_r.append(q["timeleft"])
+                queue_filtered.append(temp_r)
+
+        return queue_filtered
+
+    def print_row(self, row, y):
+        self.columns_size = map(lambda x: round(x[1]*self.maxX), self.columns)
+        q = zip(row, self.columns_size)
+        curX = 0
+        for (row_element, size) in q:
+            self.window.addstr(y, curX, row_element[:size])
+            curX += size + self.padding
 
     def display(self):
         self.window.erase()
         self.update()
-        #self.window.touchwin()
         self.window.noutrefresh()
 
     def process_key(self, key):
